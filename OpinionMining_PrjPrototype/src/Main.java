6b8bdb5ef4086fcca50af82171515ae5cc1c6027
Lavinia Bobonete <lavinia.bobonete@endava.com>
2014-02-02 18:05:49 +0200

    Added new amazon review

--- OpinionMining_PrjPrototype/src/Main.java
+++ OpinionMining_PrjPrototype/src/Main.java
@@ -1,15 +1,36 @@
 import results.ReviewExtraction;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    private static final String WORKING_DIR_PATH = System.getProperty("user.dir") + "\\resources\\reviews";
 
     public static void main(String args[]) {
 
-        ReviewExtraction nexusReview = new ReviewExtraction("nexus4.txt");
-        nexusReview.extractResults();
-        nexusReview.printResults();
+        List<String> fileNames = new ArrayList<String>();
+        File[] files = new File(WORKING_DIR_PATH).listFiles();
 
-        ReviewExtraction samsungReview = new ReviewExtraction("samsungS4.txt");
-        samsungReview.extractResults();
-        samsungReview.printResults();
+        for (File file : files) {
+            if (file.isFile()) {
+                fileNames.add(file.getName());
+            }
+        }
+
+        for(String fileName : fileNames) {
+            ReviewExtraction review = new ReviewExtraction(fileName);
+            review.extractResults();
+            review.printResults();
+        }
+
+//        ReviewExtraction nexusReview = new ReviewExtraction("nexus4.txt");
+//        nexusReview.extractResults();
+//        nexusReview.printResults();
+//
+//        ReviewExtraction samsungReview = new ReviewExtraction("samsungS4.txt");
+//        samsungReview.extractResults();
+//        samsungReview.printResults();
     }
 }
